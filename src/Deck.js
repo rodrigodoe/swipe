bf7a632aa0407a898d583bb312233ed2286a3722
Rodrigo Carvalho <roda7x@gmail.com>
2018-04-19 23:15:03 -0300

    first commit

---
+++ src/Deck.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Animated,
+  PanResponder
+} from 'react-native';
+
+
+class Deck extends Component {
+  constructor(props) {
+    super(props);
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => { },
+      onPanResponderMove: () => { },
+      onPanResponderRelease: () => { }
+    });
+    this.state = { panResponder };
+  }
+
+
+  renderCards() {
+    return this.props.data.map(item => {
+      return this.props.renderCard(item);
+    });
+  }
+
+  render() {
+    return (
+      <View >
+        {this.renderCards()}
+      </View>
+    );
+  }
+};
+
+export default Deck;
